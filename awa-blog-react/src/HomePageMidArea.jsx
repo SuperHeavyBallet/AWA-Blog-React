fb0986ac168b4b3d448feab47e0802302bed7b01
Alexander Gorham <agram@hotmail.co.uk>
2024-03-28 03:29:14 +0100

    Mid area update

--- awa-blog-react/src/HomePageMidArea.jsx
+++ awa-blog-react/src/HomePageMidArea.jsx
@@ -4,6 +4,11 @@ export default function HomePageMidArea()
     return (
         <div className="home-page-mid-area">
             <h3>MidArea</h3>
+            <div className="mid-area-columns">
+            <div><h3>1</h3></div>
+            <div><h3>2</h3></div>
+            <div><h3>3</h3></div>
+            </div>
         </div>
     )
 }
