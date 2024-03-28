62f582a01ae7650d97bdf1031183d848938374d7
Alexander Gorham <agram@hotmail.co.uk>
2024-03-28 03:23:25 +0100

    Set up mid area home page

---
+++ awa-blog-react/src/HomePageMidArea.jsx
@@ -0,0 +1,9 @@
+export default function HomePageMidArea()
+{
+
+    return (
+        <div className="home-page-mid-area">
+            <h3>MidArea</h3>
+        </div>
+    )
+}
--- awa-blog-react/src/MainColumn.jsx
+++ awa-blog-react/src/MainColumn.jsx
@@ -1,10 +1,12 @@
 import HomePageHeadArea from "./HomePageHeadArea";
+import HomePageMidArea from "./HomePageMidArea";
 
 export default function MainColumn()
 {
     return(
         <div className="main-column">
             <HomePageHeadArea />
+            <HomePageMidArea />
             <HomePageHeadArea />
             <HomePageHeadArea />
             
